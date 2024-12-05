a52b32c1e576a4913841908002c734722069ea0a
Thijs De Laet <s151582@ap.be>
2024-12-05 15:19:51 +0100

    Added collision layer to map + rebuild the content

--- GameDevProject/obj/Debug/net6.0/GameDevProject.AssemblyInfo.cs
+++ GameDevProject/obj/Debug/net6.0/GameDevProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaebb0a160275784ed84c2d683e44cbd14697e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+780d1efb191f834211fc876194614be2f2dc4371")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameDevProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
