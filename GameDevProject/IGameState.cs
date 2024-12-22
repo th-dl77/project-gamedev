8a15827263b3059fb34b6228f1c64d1df0bccbf2
Thijs De Laet <s151582@ap.be>
2024-12-23 00:30:52 +0100

    Add IGameState interface

---
+++ GameDevProject/IGameState.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject
+{
+    public interface IGameState
+    {
+        void Update(Game1 game, GameTime gameTime);
+        void Draw(Game1 game, GameTime gameTime);
+    }
+}
