6f46f256b0915e6edd80c3833a33f0673ed65552
Thijs De Laet <s151582@ap.be>
2024-12-22 15:32:50 +0100

    Working map drawing with wrong textures

--- GameDevProject/Game1.cs
+++ GameDevProject/Game1.cs
@@ -6,6 +6,9 @@ using System.Runtime.CompilerServices;
 using GameDevProject.Collisions;
 using GameDevProject.Entities;
 using GameDevProject.Input;
+using GameDevProject.Map;
+using System.Runtime.ExceptionServices;
+using System.IO;
 
 namespace GameDevProject
 {
@@ -15,10 +18,15 @@ namespace GameDevProject
         private SpriteBatch _spriteBatch;
 
         private Camera _camera;
+        private string[,] tileMap;
+        private const int MAP_WIDTH = 50;
+        private const int TILE_SIZE = 32;
+        private const int MAP_HEIGHT = 50;
+        private Texture2D[] tiles;
+
+
 
         private CollisionManager _collisionManager;
-
-        private Texture2D tileMap;
 
         private List<IEntity> entities;
         private EnemyFactory enemyFactory;
@@ -54,19 +62,27 @@ namespace GameDevProject
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            tileMap = Content.Load<Texture2D>("tileMapTextures");
-            
-            enemyFactory = new EnemyFactory(Content);
-            for (int i = 0; i < 1000; i+=100)
+            // Load tile textures
+            tiles = new Texture2D[3]; // Assuming 16 different tile types
+            for (int i = 0; i < tiles.Length; i++)
             {
-                entities.Add(enemyFactory.CreateEnemy(new Vector2(300+i, 400+i)));
+                tiles[i] = Content.Load<Texture2D>("tileMapTextures" + (i+1));
+            }
+
+            // Load tilemap from CSV file
+            LoadTilemap("Content/Tilemap.txt");
+
+            enemyFactory = new EnemyFactory(Content);
+            for (int i = 0; i < 1000; i += 100)
+            {
+                entities.Add(enemyFactory.CreateEnemy(new Vector2(300 + i, 400 + i)));
             }
 
             Texture2D spriteSheetTexture = Content.Load<Texture2D>("char_red_1");
 
             IInputStrategy inputStrategy = new KeyboardInputStrategy();
 
-            player = PlayerFactory.CreatePlayer(inputStrategy, spriteSheetTexture, _camera, new Vector2(200,200));
+            player = PlayerFactory.CreatePlayer(inputStrategy, spriteSheetTexture, _camera, new Vector2(200, 200));
 
             #region debug 
             //_debugTexture = new Texture2D(GraphicsDevice, 1, 1);
@@ -92,17 +108,19 @@ namespace GameDevProject
             //clear screen
             GraphicsDevice.Clear(Color.Gray);
 
-            //draw the map
-            _spriteBatch.Begin(transformMatrix: _camera.Transform);
-            /* Debug draw for collidables
-            _collisionManager.DrawCollidables(_spriteBatch);*/
-            _spriteBatch.End();
 
             //draw the player
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(tileMap, new Rectangle(0, 0, 800, 800), Color.White);
-            /* used to draw textures around the bounds of the playerchar
-            player.DrawBounds(_spriteBatch, _debugTexture);*/
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, _camera.Transform);
+
+            // Draw tiles
+            for (int y = 0; y < MAP_HEIGHT; y++)
+            {
+                for (int x = 0; x < MAP_WIDTH; x++)
+                {
+                    int tileIndex = int.Parse(tileMap[x, y]); // Get tile index from tilemap
+                    _spriteBatch.Draw(tiles[tileIndex], new Rectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), Color.White);
+                }
+            }
             player.Draw(_spriteBatch);
             foreach (var entity in entities)
             {
@@ -110,6 +128,37 @@ namespace GameDevProject
             }
             _spriteBatch.End();
             base.Draw(gameTime);
+        }
+        private void LoadTilemap(string filename)
+        {
+            tileMap = new string[MAP_WIDTH, MAP_HEIGHT];
+
+            using (var stream = TitleContainer.OpenStream("Content/Tilemap.txt"))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                int y = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] tiles = line.Split(',');
+
+                    for (int x = 0; x < MAP_WIDTH; x++)
+                    {
+                        if (x < tiles.Length) // Check if index is within bounds
+                        {
+                            tileMap[x, y] = tiles[x];
+                        }
+                        else
+                        {
+                            // Handle missing tile data (e.g., fill with default tile)
+                            tileMap[x, y] = "0";
+                        }
+                    }
+
+                    y++;
+                }
+            }
         }
     }
 }
