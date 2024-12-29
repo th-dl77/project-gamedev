fc47a15d34550893c14303562f7afa31f88a67dc
Thijs De Laet <s151582@ap.be>
2024-12-29 16:07:06 +0100

    Add GameAssets folder and class

---
+++ GameDevProject/Assets/GameAssets.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDevProject.Assets
+{
+    public class GameAssets
+    {
+        public Texture2D ButtonTexture { get; }
+        public SpriteFont Font { get; }
+        public Texture2D DeathScreenBackground { get; }
+        public Texture2D PlayerTexture { get; }
+
+        public GameAssets(Texture2D buttonTexture, SpriteFont font, Texture2D deathScreenBackground, Texture2D playerTexture)
+        {
+            ButtonTexture = buttonTexture;
+            Font = font;
+            DeathScreenBackground = deathScreenBackground;
+            PlayerTexture = playerTexture;
+        }
+    }
+}
