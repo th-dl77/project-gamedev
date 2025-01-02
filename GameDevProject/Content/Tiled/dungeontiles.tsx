fb8a690e50f543bfd255919d0a715512ee8301d7
Thijs De Laet <s151582@ap.be>
2025-01-02 19:01:25 +0100

    Removed old map-folders

--- GameDevProject/Content/Tiled/dungeontiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="dungeontiles" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="../../../../../Bureaublad/Dungeon Free/dungeon_floor.png" width="160" height="96"/>
-</tileset>
--- GameDevProject/Content/Tiled/levelbuild.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="levelbuild" tilewidth="16" tileheight="16" tilecount="2560" columns="64">
- <image source="../maptiles/mainlevbuild.png" width="1024" height="640"/>
-</tileset>
