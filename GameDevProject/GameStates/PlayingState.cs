98f94bbd654bd6db7e1961f07fafd6ca30a35b2b
Thijs De Laet <s151582@ap.be>
2025-01-03 13:33:42 +0100

    Refactor playingstate logic into seperate methods

--- GameDevProject/GameStates/PlayingState.cs
+++ GameDevProject/GameStates/PlayingState.cs
@@ -45,24 +45,42 @@ namespace GameDevProject.GameStates
             }
             if (game.player.IsHitting)
             {
-                Rectangle swordHitbox = game.player.fightingHitboxHandler.GetSwordHitbox();
-
-                foreach (var entity in game.entities)
-                {
-                    if (swordHitbox.Intersects(entity.GetBounds()))
-                    {
-                        entity.Die(gameTime);
-                    }
-                }
+                HandlePlayerHitting(game, gameTime);
             }
             game.collisionLoader.UpdateEnemyCollidables(game.entities);
             game._camera.Update(game.player.Position, 1600, 1600);
+        }
+
+        private void HandlePlayerHitting(Game1 game, GameTime gameTime)
+        {
+            Rectangle swordHitbox = game.player.fightingHitboxHandler.GetSwordHitbox();
+
+            foreach (var entity in game.entities)
+            {
+                if (swordHitbox.Intersects(entity.GetBounds()))
+                {
+                    entity.Die(gameTime);
+                }
+            }
         }
         public void Draw(Game1 game, GameTime gameTime)
         {
             SpriteBatch _spriteBatch = game._spriteBatch;
             _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, camera.Transform);
-
+            DrawTileMap(game);
+            DrawEnemies(game);
+            game.player.Draw(_spriteBatch);
+            //debug
+            //game.player.DrawBounds(_spriteBatch, gameAssets.GetTexture("debug"));
+            //game._collisionManager.DrawCollidables(_spriteBatch, gameAssets.GetTexture("debug"));
+            _spriteBatch.End();
+            _spriteBatch.Begin();
+            DrawUI(game);
+            _spriteBatch.End();
+        }
+        private void DrawTileMap(Game1 game)
+        {
+            SpriteBatch spriteBatch = game._spriteBatch;
             for (int y = 0; y < game.tileMap.GetLength(1); y++)
             {
                 for (int x = 0; x < game.tileMap.GetLength(0); x++)
@@ -72,24 +90,25 @@ namespace GameDevProject.GameStates
 
                     if (tileIndex >= 0 && tileIndex < game.tiles.Length)
                     {
-                        _spriteBatch.Draw(game.tiles[tileIndex], new Rectangle(x * Game1.TILE_SIZE, y * Game1.TILE_SIZE, Game1.TILE_SIZE, Game1.TILE_SIZE), Color.White);
+                       spriteBatch.Draw(game.tiles[tileIndex], new Rectangle(x * Game1.TILE_SIZE, y * Game1.TILE_SIZE, Game1.TILE_SIZE, Game1.TILE_SIZE), Color.White);
                     }
                 }
             }
+        }
+        private void DrawEnemies(Game1 game)
+        {
+            SpriteBatch spriteBatch = game._spriteBatch;
             foreach (var entity in game.entities)
             {
-                entity.Draw(_spriteBatch);
+                entity.Draw(spriteBatch);
             }
-            game.player.Draw(_spriteBatch);
-            //debug
-            //game.player.DrawBounds(_spriteBatch, gameAssets.GetTexture("debug"));
-            //game._collisionManager.DrawCollidables(_spriteBatch, gameAssets.GetTexture("debug"));
-            _spriteBatch.End();
-            _spriteBatch.Begin();
-            game.healthRenderer.Draw(_spriteBatch, game.player.Health, game.player.MaxHealth);
-            _spriteBatch.DrawString(gameAssets.GetFont("font"), $"Current level : {currentLevel}", new Vector2(10, 10), Color.Red,0f,new Vector2(0,0),0.4f,SpriteEffects.None,0f);
-            _spriteBatch.DrawString(gameAssets.GetFont("font"), $"Enemies left: {game.entities.Count(e => e.IsAlive)}", new Vector2(10, 50), Color.Red, 0f, new Vector2(0, 0), 0.4f, SpriteEffects.None, 0f);
-            _spriteBatch.End();
+        }
+        private void DrawUI(Game1 game)
+        {
+            SpriteBatch spriteBatch = game._spriteBatch;
+            game.healthRenderer.Draw(spriteBatch, game.player.Health, game.player.MaxHealth);
+            spriteBatch.DrawString(gameAssets.GetFont("font"), $"Current level : {currentLevel}", new Vector2(10, 10), Color.Red, 0f, new Vector2(0, 0), 0.4f, SpriteEffects.None, 0f);
+            spriteBatch.DrawString(gameAssets.GetFont("font"), $"Enemies left: {game.entities.Count(e => e.IsAlive)}", new Vector2(10, 50), Color.Red, 0f, new Vector2(0, 0), 0.4f, SpriteEffects.None, 0f);
         }
     }
 }
